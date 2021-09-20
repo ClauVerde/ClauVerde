2b32972c40995b2c92a7c5ffe74fedcf4e688c09
ClauVerde <claudiaverde182@gmail.com>
2021-09-20 01:00:30 -0300

    segundo commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,10 +15,11 @@ $(function () {
 
 $(window).scroll(function () {
     var scroll = $(window).scrollTop();
-    if (scroll < 500) {
+    if (scroll < 1000) {
       $(".fixed-top").css("background", "transparent");
     } else {
       $(".fixed-top").css("background", "#15aabf");
     }
   });
 });
+
